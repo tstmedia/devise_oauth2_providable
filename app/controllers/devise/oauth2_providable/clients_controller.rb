824f71726713280661047ab68e701e278923c2b6
Jon Phenow <j.phenow@gmail.com>
2012-10-17 11:37:24 -0500

    some notes on the client create API

--- app/controllers/devise/oauth2_providable/clients_controller.rb
+++ app/controllers/devise/oauth2_providable/clients_controller.rb
@@ -1,6 +1,8 @@
 module Devise
   module Oauth2Providable
     class ClientsController < ApplicationController
+      # TODO Ensure we lock this to internal use
+      # Will primarily be used for Elmer gem users to create a client easily
       def create
         client = Client.where(params[:client]).first || Client.new(params[:client])
         if !client.new_record? or client.save
