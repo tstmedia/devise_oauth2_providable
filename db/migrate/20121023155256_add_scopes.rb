ca8559b67a969f1c8164d8aa9e007f0ee7db6b90
Jon Phenow <j.phenow@gmail.com>
2012-10-24 17:24:14 -0500

    add timestamps

--- db/migrate/20121023155256_add_scopes.rb
+++ db/migrate/20121023155256_add_scopes.rb
@@ -3,10 +3,14 @@ class AddScopes < ActiveRecord::Migration
     create_table :oauth2_available_scopes do |t|
       t.belongs_to :client
       t.string :scope_name
+
+      t.timestamps
     end
 
     create_table :oauth2_enabled_scopes do |t|
       t.belongs_to :user, :available_scope
+
+      t.timestamps
     end
   end
 end
