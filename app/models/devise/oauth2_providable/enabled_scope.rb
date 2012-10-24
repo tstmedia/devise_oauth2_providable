3e53f7e9dcf8cf557dc723b729bf3d65c60d4a92
Jon Phenow <j.phenow@gmail.com>
2012-10-24 15:48:48 -0500

    some changes that delete old enablings on approval and create new ones, need to look at merging

--- app/models/devise/oauth2_providable/enabled_scope.rb
+++ app/models/devise/oauth2_providable/enabled_scope.rb
@@ -5,10 +5,16 @@ class Devise::Oauth2Providable::EnabledScope < ActiveRecord::Base
   delegate :client, :name, :scope,
     to: :available_scope
 
-  def self.create_or_init(user, client, scope_params)
+  def self.create_or_change(user, client, scope_params)
     available_scopes = client.available_scopes.
       inject({}) { |h,k| h[k.name.to_s] = k.id; h }
     scopes = scope_params.select { |k,v| available_scopes[k.to_s] && v.to_s == "1" }.keys
+    Devise::Oauth2Providable::EnabledScope.joins(:available_scope).where(
+      user_id: user.id,
+      oauth2_available_scopes: {
+        client_id: client.id
+      }
+    ).destroy_all
     scopes.each do |scope_name|
       enabled = new
       enabled.user_id = user.id
