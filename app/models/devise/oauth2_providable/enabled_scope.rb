58b694257ecf35d7b64b6a37da094102285e45cb
Jon Phenow <j.phenow@gmail.com>
2012-10-24 16:02:25 -0500

    keep ones that we already have - destroy others

--- app/models/devise/oauth2_providable/enabled_scope.rb
+++ app/models/devise/oauth2_providable/enabled_scope.rb
@@ -9,13 +9,21 @@ class Devise::Oauth2Providable::EnabledScope < ActiveRecord::Base
     available_scopes = client.available_scopes.
       inject({}) { |h,k| h[k.name.to_s] = k.id; h }
     scopes = scope_params.select { |k,v| available_scopes[k.to_s] && v.to_s == "1" }.keys
+    already_have = Devise::Oauth2Providable::EnabledScope.
+      joins(:available_scope).
+      where(
+        user_id: user.id,
+        oauth2_available_scopes: {
+          scope_name: scopes
+        }
+      ).collect(&:name)
     Devise::Oauth2Providable::EnabledScope.joins(:available_scope).where(
       user_id: user.id,
       oauth2_available_scopes: {
         client_id: client.id
       }
-    ).destroy_all
-    scopes.each do |scope_name|
+    ).where("oauth2_available_scopes.scope_name NOT IN (?)", scopes).destroy_all
+    (scopes - already_have).each do |scope_name|
       enabled = new
       enabled.user_id = user.id
       enabled.available_scope_id = available_scopes[scope_name]
