856a3191504d7c40561e9322abc3738c0af67707
Jon Phenow <j.phenow@gmail.com>
2012-11-08 13:21:52 -0600

    reset session before token-ing

--- app/controllers/devise/oauth2_providable/tokens_controller.rb
+++ app/controllers/devise/oauth2_providable/tokens_controller.rb
@@ -1,13 +1,11 @@
 module Devise
   module Oauth2Providable
     class TokensController < ApplicationController
-      #before_filter :authenticate_user!
+      before_filter :reset_session!
+      before_filter :authenticate_user!
       skip_before_filter :verify_authenticity_token, :only => :create
 
       def create
-        authenticate_user!
-        Rails.logger.ap current_user
-        Rails.logger.ap warden
         @refresh_token = oauth2_current_refresh_token || oauth2_current_client.refresh_tokens.create!(:user => current_user)
         @access_token = @refresh_token.access_tokens.create!(:client => oauth2_current_client, :user => current_user)
         render :json => @access_token.token_response
@@ -18,6 +16,9 @@ module Devise
       end
       def oauth2_current_refresh_token
         env[Devise::Oauth2Providable::REFRESH_TOKEN_ENV_REF]
+      end
+      def reset_session!
+        request.reset_session
       end
     end
   end
